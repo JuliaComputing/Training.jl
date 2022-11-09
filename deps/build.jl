44aac8a255bb912f195a963d6880b49f8d98cfa0
Josh Day <emailjoshday@gmail.com>
2022-11-09 14:17:10 -0500

    fix build script

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using Pluto
 
 dir = joinpath(@__DIR__, "..", "pluto_notebooks")
 
-for file in readdir(dir)
+for file in filter(x -> endswith(x, ".jl"), readdir(dir))
     Pluto.activate_notebook_environment(joinpath(dir, file))
     Pkg.instantiate()
 end
