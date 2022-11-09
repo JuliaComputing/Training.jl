04e147ad2a3bb9955e683368c61b7f5dcdbd1838
Josh Day <emailjoshday@gmail.com>
2022-11-09 12:05:19 -0500

    wip

--- src/Training.jl
+++ src/Training.jl
@@ -1,5 +1,14 @@
 module Training
 
-# Write your package code here.
+export run_jupyter, run_pluto
+
+using IJulia: IJulia
+using Pluto: Pluto
+
+run_jupyter() = IJulia.notebook(dir = joinpath(@__DIR__, "..", "jupyter_notebooks"))
+
+run_pluto() = cd(joinpath(@__DIR__, "..", "pluto_notebooks")) do
+    Pluto.run(n)
+end
 
 end
